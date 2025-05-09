a1827d525c736bddf0b8c33b34c03e330a3dadba
Elli Amir <el.ad.david.amir@gmail.com>
2025-05-09 15:03:30 -0400

    trying frontend build in heroku

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,8 @@
 call heroku config:set VITE_API_BASE_URL=https://blueprint-elli-amir-ac8042d2a76d.herokuapp.com/ --app blueprint-elli-amir
+
+call heroku buildpacks:clear --app blueprint-elli-amir
+call heroku buildpacks:add --index 1 heroku/nodejs --app blueprint-elli-amir
+call heroku buildpacks:add --index 2 heroku/python --app blueprint-elli-amir
+
 git push heroku main
 
